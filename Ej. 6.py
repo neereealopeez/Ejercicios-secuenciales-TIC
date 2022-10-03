a8ace0e000beab1ba647eec8f1290a659ae1d070
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-03 11:13:46 +0200

    Añadido ficheros 3,4,5 y 6 a medias

---
+++ Ej. 6.py
@@ -0,0 +1,7 @@
+#Sexto Ejercicio: Calcular la media de tres números pedidos por teclado.
+num1=0
+num2=0
+num3=0
+num1=(int)(input("¿Primer número?" ))
+num2=(int)(input("¿Segundo número?" ))
+num3=(int)(input("¿Tercer número?" ))
