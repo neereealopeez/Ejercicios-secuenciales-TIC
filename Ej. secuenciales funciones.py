59e5a5896bdf596968425b60614f5acd844277e0
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-18 14:41:26 +0200

    Ejercicios de funciones encima de los secuenciales

---
+++ Ej. secuenciales funciones.py
@@ -0,0 +1,7 @@
+#Primer Ejercicio: Escribir un programa que pregunte al usuario su nombre, y luego lo salude. 
+def ejer1():
+    nombre="" 
+    nombre=input("Dime tu nombre \n") 
+    print("Tu nombre es", nombre,)
+
+ejer1()
